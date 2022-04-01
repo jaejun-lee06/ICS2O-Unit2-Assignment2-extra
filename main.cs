ce325d6929a4b8d09bd2398a0ac300f31f0fb9e7
jaejun-lee06 <99355321+jaejun-lee06@users.noreply.github.com>
2022-04-01 02:40:13 +0000

    done

--- main.cs
+++ main.cs
@@ -1,7 +1,30 @@
+// Created by: Jaejun Lee
+// Created on: Mar 2022
+//
+// This program accepts user input
+
 using System;
 
-class Program {
-  public static void Main (string[] args) {
-    Console.WriteLine ("Hello World");
-  }
+class Program
+{
+    public static void Main(string[] args)
+    {
+        // This function calculates the area and perimeter of a right triangle
+        int Base;
+        int Height;
+
+        Console.WriteLine("This program calculates the perimeter of a right triangle.");
+        Console.WriteLine("");
+        Console.Write("Enter the base (cm): ");
+        Base = Convert.ToInt32(Console.ReadLine());
+        Console.Write("Enter the height (cm): ");
+        Height = Convert.ToInt32(Console.ReadLine());
+
+        Double perimeter = Height + Base + Math.Sqrt(Height * Height + Base * Base);
+
+        Console.WriteLine("");
+        Console.Write("Perimeter of the right triangle = " + perimeter + " cm");
+        Console.ReadKey();
+        Console.WriteLine("\nDone.");
+    }
 }
