7479650c2f658f04a441228c2a4bcfb7c699e6ef
jaejun-lee06 <99355321+jaejun-lee06@users.noreply.github.com>
2022-04-05 17:24:56 +0000

    fixx

--- main.cs
+++ main.cs
@@ -12,6 +12,7 @@ class Program
         // This function calculates the area and perimeter of a right triangle
         int Base;
         int Height;
+        int perimeter;
 
         Console.WriteLine("This program calculates the perimeter of a right triangle.");
         Console.WriteLine("");
@@ -20,7 +21,7 @@ class Program
         Console.Write("Enter the height (cm): ");
         Height = Convert.ToInt32(Console.ReadLine());
 
-        Double perimeter = Height + Base + Math.Sqrt(Height * Height + Base * Base);
+        perimeter = Height + Base + Math.Sqrt(Height * Height + Base * Base);
 
         Console.WriteLine("");
         Console.Write("Perimeter of the right triangle = " + perimeter + " cm");
